19201bb3c6d1674e37ed7c657e64bca5979e79fa
shehu14 <45355219+shehu14@users.noreply.github.com>
2020-08-05 17:58:48 +0100

    update

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function changePic() {
-  document.getElementById("profilePic").src = "images/adrian.webp";
+  document.getElementById("profilePic").src = "images/IMG_3671";
 }
 
 function normalPic() {
-  document.getElementById("profilePic").src = "images/adrian.jpg";
+  document.getElementById("profilePic").src = "images/IMG_4980.JPG";
 }
 
-
+/*
 $(".go-to-contact").click(function() {
   $('html, body').animate({
       scrollTop: $(".contact").offset().top
   }, 1000);
-});
+});*/
